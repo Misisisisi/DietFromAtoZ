f3a43dd84a81b8db2a273287b95f09e369f58940
misisisisi <aniazajac8@gmail.com>
2022-05-20 20:24:14 +0200

    Initial commit

---
+++ src/test/java/com/github/misisisisi/dietfromatoz/DietFromAtoZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.misisisisi.dietfromatoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietFromAtoZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
