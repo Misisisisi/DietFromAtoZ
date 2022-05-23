87d4441f1ad558ee1b1381a5b1dfe5a2c39c6542
misisisisi <aniazajac8@gmail.com>
2022-05-23 23:23:11 +0200

    Add LoginController

--- src/main/java/com/github/misisisisi/dietfromatoz/controller/LoginController.java
+++ src/main/java/com/github/misisisisi/dietfromatoz/controller/LoginController.java
@@ -1,23 +1,36 @@
 package com.github.misisisisi.dietfromatoz.controller;
 
+import com.github.misisisisi.dietfromatoz.model.UserEntity;
 import com.github.misisisisi.dietfromatoz.repository.UserRepository;
+import com.github.misisisisi.dietfromatoz.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/login")
 @RequiredArgsConstructor
 public class LoginController {
 
-    private final UserRepository userRepository;
-    @GetMapping
+    private final UserService userService;
+
     public String login(Model model){
         model.addAttribute("createUserForm", new CreateUserForm());
         return "userInterface/login";
     }
-
-
+    @GetMapping
+    public String processLogin(@ModelAttribute("createUserForm") @Valid CreateUserForm createUserForm, BindingResult result) {
+        if (result.hasErrors()) {
+            return "userInterface/login";
+        } else {
+            userService.saveUser(createUserForm);
+            return "dashboard/homePage";
+        }
+    }
 }
