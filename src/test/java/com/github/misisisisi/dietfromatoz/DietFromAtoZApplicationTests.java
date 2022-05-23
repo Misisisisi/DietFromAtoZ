076fec2d78221c65fde4209b34a01197bd8ef2cf
misisisisi <aniazajac8@gmail.com>
2022-05-23 15:59:51 +0200

    DietFromAtoZApplication.tests

--- src/test/java/com/github/misisisisi/dietfromatoz/DietFromAtoZApplicationTests.java
+++ src/test/java/com/github/misisisisi/dietfromatoz/DietFromAtoZApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DietFromAtoZApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
