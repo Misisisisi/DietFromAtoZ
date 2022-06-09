3774dbc825cfdf6b70177a6265bec2f1b9bf7216
misisisisi <aniazajac8@gmail.com>
2022-06-09 22:11:49 +0200

    add RoleEntity

---
+++ src/main/java/com/github/misisisisi/dietfromatoz/model/RoleEntity.java
@@ -0,0 +1,19 @@
+package com.github.misisisisi.dietfromatoz.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="roles")
+@Getter
+@Setter
+@ToString @EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor @Builder
+public class RoleEntity {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
