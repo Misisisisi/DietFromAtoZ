34036ac941cbfba90b2bf2c8668017895d8e4ab0
misisisisi <aniazajac8@gmail.com>
2022-06-09 22:00:03 +0200

    getting logged user and saving them to added product

--- src/main/java/com/github/misisisisi/dietfromatoz/controller/CreateProductController.java
+++ src/main/java/com/github/misisisisi/dietfromatoz/controller/CreateProductController.java
@@ -1,9 +1,14 @@
 package com.github.misisisisi.dietfromatoz.controller;
 
 import com.github.misisisisi.dietfromatoz.model.ProductEntity;
+import com.github.misisisisi.dietfromatoz.model.UserEntity;
+import com.github.misisisisi.dietfromatoz.repository.UserRepository;
 import com.github.misisisisi.dietfromatoz.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -21,6 +26,7 @@ import javax.validation.Valid;
 public class CreateProductController {
 
     private final ProductService productService;
+    private final UserRepository userRepository;
 
     @GetMapping
     public String addNewProduct(Model model) {
@@ -33,7 +39,11 @@ public class CreateProductController {
         if (result.hasErrors()) {
             return "product/add";
         } else {
-            productService.saveProduct(createProductForm);
+            UsernamePasswordAuthenticationToken authentication = (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+            org.springframework.security.core.userdetails.UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            UserEntity username = userRepository.findUserByUsername(userDetails.getUsername());
+
+            productService.saveProduct(createProductForm, username);
             return "product/added";
         }
     }
