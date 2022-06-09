a08343dbba58eea732fc54496042dbfb206cd7d7
misisisisi <aniazajac8@gmail.com>
2022-06-09 22:22:00 +0200

    update ProductService

--- src/main/java/com/github/misisisisi/dietfromatoz/service/ProductService.java
+++ src/main/java/com/github/misisisisi/dietfromatoz/service/ProductService.java
@@ -1,11 +1,12 @@
 package com.github.misisisisi.dietfromatoz.service;
 
 import com.github.misisisisi.dietfromatoz.controller.CreateProductForm;
+import com.github.misisisisi.dietfromatoz.model.UserEntity;
 import org.springframework.transaction.annotation.Transactional;
 
 
 public interface ProductService {
 
     @Transactional
-    void saveProduct(CreateProductForm createProductForm);
+    void saveProduct(CreateProductForm createProductForm, UserEntity userEntity);
 }
