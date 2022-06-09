48a24d32aebc66f85c7e976874fdd04178434c73
misisisisi <aniazajac8@gmail.com>
2022-06-09 22:12:23 +0200

    add ResultEnergyRepository

---
+++ src/main/java/com/github/misisisisi/dietfromatoz/repository/ResultEnergyRepository.java
@@ -0,0 +1,8 @@
+package com.github.misisisisi.dietfromatoz.repository;
+
+import com.github.misisisisi.dietfromatoz.model.ResultsEnergyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResultEnergyRepository extends JpaRepository<ResultsEnergyEntity, Long> {
+
+}
