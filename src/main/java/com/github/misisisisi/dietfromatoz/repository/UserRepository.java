d36b36de8c040b4daffa92b4b14643ca0fca1b85
misisisisi <aniazajac8@gmail.com>
2022-06-09 22:12:53 +0200

    update UserRepository

--- src/main/java/com/github/misisisisi/dietfromatoz/repository/UserRepository.java
+++ src/main/java/com/github/misisisisi/dietfromatoz/repository/UserRepository.java
@@ -10,7 +10,7 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByFirstNameAndLastName (String firstName, String lastName);
 
-    Optional<UserEntity> findByUsername(String username);
+    Optional<UserEntity> findByUsernameIgnoreCase(String username);
 
     Optional<UserEntity> findById(Long id);
 
