5e2a5701e21629f474674f5a6bb540ff348848c1
misisisisi <aniazajac8@gmail.com>
2022-06-10 21:45:27 +0200

    add query to PlanMealRepository

--- src/main/java/com/github/misisisisi/dietfromatoz/repository/PlanMealRepository.java
+++ src/main/java/com/github/misisisisi/dietfromatoz/repository/PlanMealRepository.java
@@ -2,6 +2,7 @@ package com.github.misisisisi.dietfromatoz.repository;
 
 import com.github.misisisisi.dietfromatoz.model.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +11,8 @@ import java.util.Optional;
 public interface PlanMealRepository extends JpaRepository<ProductEntity, Long> {
 
     ProductEntity findByProductName (String name);
+
+    @Query("SELECT productEntity FROM ProductEntity productEntity WHERE productEntity.productName = :productName AND (productEntity.owner.username=:username OR productEntity.owner.username='admin')")
+    ProductEntity findProductByNameAndUserName(String productName, String username);
+
 }
