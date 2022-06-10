bbbc2d74519ac3aade21e0c5e44b1acf97601aac
misisisisi <aniazajac8@gmail.com>
2022-06-10 21:44:41 +0200

    add Username to query AddProductToMealRepository

--- src/main/java/com/github/misisisisi/dietfromatoz/repository/AddProductToMealRepository.java
+++ src/main/java/com/github/misisisisi/dietfromatoz/repository/AddProductToMealRepository.java
@@ -1,6 +1,7 @@
 package com.github.misisisisi.dietfromatoz.repository;
 
 import com.github.misisisisi.dietfromatoz.model.ProductOfMealEntity;
+import com.github.misisisisi.dietfromatoz.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,26 +9,22 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface AddProductToMealRepository extends JpaRepository<ProductOfMealEntity, Long> {
-//
-//    @Query("SELECT productOfMeal FROM ProductOfMealEntity  productOfMeal WHERE  productOfMeal.dayName.dayName = :dayName AND productOfMeal.mealName.mealName = :mealName")
-//    List<ProductOfMealEntity> findAllByDayNameAndMealName(@Param("dayName")String dayName, @Param("mealName")String mealName);
 
+    @Query("SELECT productOfMeal FROM ProductOfMealEntity productOfMeal WHERE productOfMeal.mealName.mealName = 'Śniadanie' AND productOfMeal.dayName.dayName = :dayName AND productOfMeal.owner.username=:username")
+    List<ProductOfMealEntity> findAllBreakfastFromDay(@Param("dayName")String dayName, String username);
 
-    @Query("SELECT productOfMeal FROM ProductOfMealEntity productOfMeal WHERE productOfMeal.mealName.mealName = 'Śniadanie' AND productOfMeal.dayName.dayName = :dayName")
-    List<ProductOfMealEntity> findAllBreakfastFromDay(@Param("dayName")String dayName);
+    @Query("SELECT productOfMeal FROM ProductOfMealEntity productOfMeal WHERE productOfMeal.mealName.mealName = 'II śniadanie' AND productOfMeal.dayName.dayName = :dayName AND productOfMeal.owner.username=:username")
+    List<ProductOfMealEntity> findAllSecondBreakfastFromDay(@Param("dayName")String dayName, String username);
 
-    @Query("SELECT productOfMeal FROM ProductOfMealEntity productOfMeal WHERE productOfMeal.mealName.mealName = 'II śniadanie' AND productOfMeal.dayName.dayName = :dayName")
-    List<ProductOfMealEntity> findAllSecondBreakfastFromDay(@Param("dayName")String dayName);
+    @Query("SELECT productOfMeal FROM ProductOfMealEntity productOfMeal WHERE productOfMeal.mealName.mealName = 'Obiad' AND productOfMeal.dayName.dayName = :dayName AND productOfMeal.owner.username=:username")
+    List<ProductOfMealEntity> findAlLunchFromDay(@Param("dayName")String dayName, String username);
 
-    @Query("SELECT productOfMeal FROM ProductOfMealEntity productOfMeal WHERE productOfMeal.mealName.mealName = 'Obiad' AND productOfMeal.dayName.dayName = :dayName")
-    List<ProductOfMealEntity> findAlLunchFromDay(@Param("dayName")String dayName);
+    @Query("SELECT productOfMeal FROM ProductOfMealEntity productOfMeal WHERE productOfMeal.mealName.mealName = 'Podwieczorek' AND productOfMeal.dayName.dayName = :dayName AND productOfMeal.owner.username=:username")
+    List<ProductOfMealEntity> findAlTeaFromDay(@Param("dayName")String dayName, String username);
 
-    @Query("SELECT productOfMeal FROM ProductOfMealEntity productOfMeal WHERE productOfMeal.mealName.mealName = 'Podwieczorek' AND productOfMeal.dayName.dayName = :dayName")
-    List<ProductOfMealEntity> findAlTeaFromDay(@Param("dayName")String dayName);
+    @Query("SELECT productOfMeal FROM ProductOfMealEntity productOfMeal WHERE productOfMeal.mealName.mealName = 'Kolacja' AND productOfMeal.dayName.dayName = :dayName AND productOfMeal.owner.username=:username")
+    List<ProductOfMealEntity> findAlDinnerFromDay(@Param("dayName")String dayName, String username);
 
-    @Query("SELECT productOfMeal FROM ProductOfMealEntity productOfMeal WHERE productOfMeal.mealName.mealName = 'Kolacja' AND productOfMeal.dayName.dayName = :dayName")
-    List<ProductOfMealEntity> findAlDinnerFromDay(@Param("dayName")String dayName);
-
-    @Query("SELECT productOfMeal FROM ProductOfMealEntity productOfMeal WHERE productOfMeal.dayName.dayName = :dayName")
-    List<ProductOfMealEntity> findAllProductsFromDay(@Param("dayName")String dayName);
+    @Query("SELECT productOfMeal FROM ProductOfMealEntity productOfMeal WHERE productOfMeal.dayName.dayName = :dayName AND productOfMeal.owner.username=:username")
+    List<ProductOfMealEntity> findAllProductsFromDay(@Param("dayName")String dayName, String username);
 }
