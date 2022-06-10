063025f3fa08154f232e3e6cfd8bdcf8012087ae
misisisisi <aniazajac8@gmail.com>
2022-06-10 21:45:12 +0200

    add query to PersonRepository

--- src/main/java/com/github/misisisisi/dietfromatoz/repository/PersonRepository.java
+++ src/main/java/com/github/misisisisi/dietfromatoz/repository/PersonRepository.java
@@ -1,7 +1,9 @@
 package com.github.misisisisi.dietfromatoz.repository;
 
 import com.github.misisisisi.dietfromatoz.model.PersonDataEntity;
+import com.github.misisisisi.dietfromatoz.model.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,9 @@ import java.util.List;
 public interface PersonRepository extends JpaRepository<PersonDataEntity, Long> {
 
     List<PersonDataEntity> findAllByUser_username(String username);
+
+    @Query("SELECT personData FROM PersonDataEntity personData WHERE personData.user.username = :username")
+    PersonDataEntity findPersonDataEntityByUserName(String username);
+
+
 }
