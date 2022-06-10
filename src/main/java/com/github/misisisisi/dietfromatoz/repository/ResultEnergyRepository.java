2602084927ba2c77263ea9e4ffcbc04155a78f46
misisisisi <aniazajac8@gmail.com>
2022-06-10 21:45:40 +0200

    add query to ResultEnergyRepository

--- src/main/java/com/github/misisisisi/dietfromatoz/repository/ResultEnergyRepository.java
+++ src/main/java/com/github/misisisisi/dietfromatoz/repository/ResultEnergyRepository.java
@@ -1,8 +1,17 @@
 package com.github.misisisisi.dietfromatoz.repository;
 
+import com.github.misisisisi.dietfromatoz.model.PersonDataEntity;
 import com.github.misisisisi.dietfromatoz.model.ResultsEnergyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ResultEnergyRepository extends JpaRepository<ResultsEnergyEntity, Long> {
+
+    List<ResultsEnergyEntity> findAllByOwner_username(String username);
+    @Query("SELECT resultEnergy FROM ResultsEnergyEntity resultEnergy WHERE resultEnergy.owner.username = :username")
+    ResultsEnergyEntity findResultEnergyEntityByUserName(String username);
+
 
 }
