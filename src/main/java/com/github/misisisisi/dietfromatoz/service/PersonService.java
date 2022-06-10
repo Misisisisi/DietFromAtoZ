5f61689379cd50cf2be148baf92bb5cdfab8a826
misisisisi <aniazajac8@gmail.com>
2022-06-10 21:50:36 +0200

    add update method to PersonService

--- src/main/java/com/github/misisisisi/dietfromatoz/service/PersonService.java
+++ src/main/java/com/github/misisisisi/dietfromatoz/service/PersonService.java
@@ -2,6 +2,7 @@ package com.github.misisisisi.dietfromatoz.service;
 
 import com.github.misisisisi.dietfromatoz.controller.CreatePersonForm;
 import com.github.misisisisi.dietfromatoz.model.PersonDataEntity;
+import com.github.misisisisi.dietfromatoz.model.UserEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,5 +12,8 @@ public interface PersonService {
 //    void savePersonData (PersonDataEntity personData);
 
     @Transactional
-    void savePersonData(CreatePersonForm createPersonForm);
+    void savePersonData(CreatePersonForm createPersonForm, UserEntity user);
+
+    @Transactional
+    void updateData(CreatePersonForm createPersonForm, UserEntity user);
 }
